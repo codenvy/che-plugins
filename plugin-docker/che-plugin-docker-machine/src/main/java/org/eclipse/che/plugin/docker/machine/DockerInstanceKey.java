58d509295bd5f9310f899ed78f4360abd0984ea3
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-05-21 15:10:50 +0300

    rename InstanceSnapshotKey to InstanceKey

--- plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstanceKey.java
+++ plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstanceKey.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.docker.machine;
 
-import org.eclipse.che.api.machine.server.spi.InstanceSnapshotKey;
+import org.eclipse.che.api.machine.server.spi.InstanceKey;
 import org.eclipse.che.dto.server.DtoFactory;
 import org.eclipse.che.dto.shared.JsonStringMap;
 
@@ -23,7 +23,7 @@ import java.util.Map;
  *
  * @author andrew00x
  */
-public class DockerInstanceSnapshotKey implements InstanceSnapshotKey {
+public class DockerInstanceKey implements InstanceKey {
     public static final String REPOSITORY = "repository";
     public static final String TAG        = "tag";
     public static final String ID         = "id";
@@ -31,15 +31,15 @@ public class DockerInstanceSnapshotKey implements InstanceSnapshotKey {
 
     private final Map<String, String> fields;
 
-    public DockerInstanceSnapshotKey(InstanceSnapshotKey instanceSnapshotKey) {
+    public DockerInstanceKey(InstanceKey instanceKey) {
         fields = new HashMap<>(4);
-        fields.put(REPOSITORY, instanceSnapshotKey.getFields().get(REPOSITORY));
-        fields.put(TAG, instanceSnapshotKey.getFields().get(TAG));
-        fields.put(ID, instanceSnapshotKey.getFields().get(ID));
-        fields.put(REGISTRY, instanceSnapshotKey.getFields().get(REGISTRY));
+        fields.put(REPOSITORY, instanceKey.getFields().get(REPOSITORY));
+        fields.put(TAG, instanceKey.getFields().get(TAG));
+        fields.put(ID, instanceKey.getFields().get(ID));
+        fields.put(REGISTRY, instanceKey.getFields().get(REGISTRY));
     }
 
-    public DockerInstanceSnapshotKey(String repository, String tag, String id, String registry) {
+    public DockerInstanceKey(String repository, String tag, String id, String registry) {
         fields = new HashMap<>(4);
         fields.put(REPOSITORY, repository);
         fields.put(TAG, tag);
@@ -80,7 +80,7 @@ public class DockerInstanceSnapshotKey implements InstanceSnapshotKey {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        DockerInstanceSnapshotKey that = (DockerInstanceSnapshotKey)o;
+        DockerInstanceKey that = (DockerInstanceKey)o;
 
         if (fields != null ? !fields.equals(that.fields) : that.fields != null) return false;
 
