af3894dc4a4ba7673f4f5fcf36fabafa8e11c7d4
Valeriy Svydenko <vsvydenko@codenvy.com>
2015-08-28 16:30:48 +0300

    added an ability to save git committer name and email into user preferences

--- plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/GitLocalizationConstant.java
+++ plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/GitLocalizationConstant.java
@@ -662,4 +662,10 @@ public interface GitLocalizationConstant extends Messages {
      */
     @Key("project.name")
     String projectName();
+
+    @Key("committer.preference.category")
+    String committerPreferenceCategory();
+
+    @Key("committer.title")
+    String committerTitle();
 }
