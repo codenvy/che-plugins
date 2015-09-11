fa5fd46ca333b4a972112a923fb74c63ae942a63
Max Shaposhnik <mshaposhnik@codenvy.com>
2015-09-11 15:39:40 +0300

    IDEX-3038; chande namings from codenvy to che;

--- plugin-git/che-plugin-git-provider-che/src/main/java/org/eclipse/che/ide/ext/git/server/nativegit/CheAccessTokenCredentialProvider.java
+++ plugin-git/che-plugin-git-provider-che/src/main/java/org/eclipse/che/ide/ext/git/server/nativegit/CheAccessTokenCredentialProvider.java
@@ -30,35 +30,38 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.eclipse.che.dto.server.DtoFactory.newDto;
 
 /**
- * Credentials provider for Codenvy
+ * Credentials provider for Che
  *
  * @author Alexander Garagatyi
  * @author Valeriy Svydenko
  */
 @Singleton
-public class CodenvyAccessTokenCredentialProvider implements CredentialsProvider {
-    private final String        codenvyHost;
+public class CheAccessTokenCredentialProvider implements CredentialsProvider {
+    private final String        cheHostName;
     private       PreferenceDao preferenceDao;
 
     @Inject
-    public CodenvyAccessTokenCredentialProvider(@Named("api.endpoint") String apiEndPoint,
-                                                PreferenceDao preferenceDao) throws URISyntaxException {
+    public CheAccessTokenCredentialProvider(@Named("api.endpoint") String apiEndPoint,
+                                            PreferenceDao preferenceDao) throws URISyntaxException {
         this.preferenceDao = preferenceDao;
-        this.codenvyHost = new URI(apiEndPoint).getHost();
+        this.cheHostName = new URI(apiEndPoint).getHost();
     }
 
     @Override
     public UserCredential getUserCredential() throws GitException {
-        String token = EnvironmentContext.getCurrent().getUser().getToken();
+        String token = EnvironmentContext.getCurrent()
+                                         .getUser()
+                                         .getToken();
         if (token != null) {
-            return new UserCredential(token, "x-codenvy", "codenvy");
+            return new UserCredential(token, "x-che", "che_password");
         }
         return null;
     }
 
     @Override
     public GitUser getUser() throws GitException {
-        User user = EnvironmentContext.getCurrent().getUser();
+        User user = EnvironmentContext.getCurrent()
+                                      .getUser();
         GitUser gitUser = newDto(GitUser.class);
         if (user.isTemporary()) {
             gitUser.setEmail("anonymous@noemail.com");
@@ -67,7 +70,9 @@ public class CodenvyAccessTokenCredentialProvider implements CredentialsProvider
             String name = null;
             String email = null;
             try {
-                Map<String, String> preferences = preferenceDao.getPreferences(EnvironmentContext.getCurrent().getUser().getId(),
+                Map<String, String> preferences = preferenceDao.getPreferences(EnvironmentContext.getCurrent()
+                                                                                                 .getUser()
+                                                                                                 .getId(),
                                                                                "git.committer.\\w+");
                 name = preferences.get("git.committer.name");
                 email = preferences.get("git.committer.email");
@@ -89,7 +94,7 @@ public class CodenvyAccessTokenCredentialProvider implements CredentialsProvider
 
     @Override
     public boolean canProvideCredentials(String url) {
-        return url.contains(codenvyHost);
+        return url.contains(cheHostName);
     }
 
 }
