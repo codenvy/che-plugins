0457ff403c25d9d1116828a8151e795c84d705b1
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-09-22 18:31:35 +0300

    IDEX-3060: detect containers stop/oom and produce events about instance stop to event service

---
+++ plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstanceStopDetector.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.docker.machine;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import org.eclipse.che.api.core.notification.EventService;
+import org.eclipse.che.api.machine.server.InstanceStateEvent;
+import org.eclipse.che.plugin.docker.client.DockerConnector;
+import org.eclipse.che.plugin.docker.client.MessageProcessor;
+import org.eclipse.che.plugin.docker.client.json.Event;
+import org.eclipse.che.plugin.docker.client.json.Filters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Track docker containers events to detect containers stop or failure.
+ *
+ * @author Alexander Garagatyi
+ */
+@Singleton // should be eager
+public class DockerInstanceStopDetector {
+    private static final Logger LOG = LoggerFactory.getLogger(DockerInstanceStopDetector.class);
+
+    private final EventService                      eventService;
+    private final DockerConnector                   dockerConnector;
+    private final ExecutorService                   executorService;
+    private final ConcurrentHashMap<String, String> instances;
+
+    private long lastProcessedEventDate = 0;
+
+    @Inject
+    public DockerInstanceStopDetector(EventService eventService, DockerConnector dockerConnector) {
+        this.eventService = eventService;
+        this.dockerConnector = dockerConnector;
+        this.instances = new ConcurrentHashMap<>();
+        this.executorService = Executors.newSingleThreadExecutor(
+                new ThreadFactoryBuilder().setNameFormat("DockerInstanceStopDetector-%d")
+                                          .setDaemon(true)
+                                          .build());
+    }
+
+    public void startDetection(String containerId, String machineId) {
+        instances.put(containerId, machineId);
+    }
+
+    public void stopDetection(String containerId) {
+        instances.remove(containerId);
+    }
+
+    @PostConstruct
+    private void detectContainersEvents() {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        dockerConnector.getEvents(lastProcessedEventDate,
+                                                  0,
+                                                  new Filters().withFilter("event", "die", "oom"),
+                                                  new EventsProcessor());
+                    } catch (IOException ignore) {
+                        // usually connection timeout
+                    }
+                }
+            }
+        });
+    }
+
+    private class EventsProcessor implements MessageProcessor<Event> {
+        @Override
+        public void process(Event message) {
+            InstanceStateEvent.Type instanceStateChangeType;
+            switch (message.getStatus()) {
+                case "oom":
+                    instanceStateChangeType = InstanceStateEvent.Type.OOM;
+                    break;
+                case "die":
+                    instanceStateChangeType = InstanceStateEvent.Type.DIE;
+                    break;
+                default:
+                    LOG.error("Unknown state. " + message.toString());
+                    return;
+            }
+            final String instanceId = instances.get(message.getId());
+            if (instanceId != null) {
+                eventService.publish(new InstanceStateEvent(instanceId, instanceStateChangeType));
+                lastProcessedEventDate = message.getTime();
+            }
+        }
+    }
+}
